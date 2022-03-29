aec03f86108fc5051d23caa389afb93f572e7c2c
sblanco <sblanco@hemav.com>
2022-03-29 19:21:41 +0200

    update readme and add rofi command

--- scripts/create-git-project.sh
+++ scripts/create-git-project.sh
@@ -12,16 +12,12 @@ else
     echo "Creating new folder and new git repository: $folder"
 fi
 
-token=$(git config --global user.token)
 user=$(git config --global user.name)
-repo=$(printf '{"name":"%s","private":false}' $folder)
-echo $repo
 
 touch README.md
 git init
 git add README.md
 git commit -m "first commit"
-git remote add origin git@github.com:magicmcallister/$folder.git
-curl -u $user:$token https://api.github.com/user/repos -d $repo
+git remote add origin git@github.com:$user/$folder.git
 git branch -m master main
 git push -u origin main
